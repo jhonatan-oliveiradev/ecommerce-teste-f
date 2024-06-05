1fc8f9471a9e1f9395bcb5489fbfb223275e5690
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-06-04 21:05:22 -0300

    feat: add Suspense to ensure that search page render correctly

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { Suspense } from "react";
 import { useSearchParams } from "next/navigation";
 import Products from "@/components/products";
 import Search from "@/components/search";
@@ -9,10 +10,18 @@ const SearchPage = () => {
   const query = searchParams.get("q");
 
   return (
-    <section className="mx-auto h-auto w-full max-w-7xl">
-      <Search />
-      {query && <Products searchQuery={query} />}
-    </section>
+    <Suspense
+      fallback={
+        <div className="flex h-screen items-center justify-center">
+          <div className="h-32 w-32 animate-spin rounded-full border-b-2 border-t-2 border-gray-900"></div>
+        </div>
+      }
+    >
+      <section className="mx-auto h-auto w-full max-w-7xl">
+        <Search />
+        {query && <Products searchQuery={query} />}
+      </section>
+    </Suspense>
   );
 };
 
