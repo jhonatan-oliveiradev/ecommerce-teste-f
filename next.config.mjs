caf7221fe240603aab9ecffc0307660719eba26f
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-06-06 18:35:18 -0300

    chore: removing unused host images domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,24 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [
-      { hostname: "i.imgur.com" },
-      { hostname: "whcamera.com" },
-      { hostname: "i2.wp.com" },
-      { hostname: "cdn.awsli.com.br" },
-      { hostname: "images3.kabum.com.br" },
-      { hostname: "www.saldaodainformatica.com.br" },
-      { hostname: "m.media-amazon.com" },
-      { hostname: "gazin-marketplace.s3.amazonaws.com" },
-      { hostname: "imperiallgames.com" },
-      { hostname: "down-br.img.susercontent.com" },
-      { hostname: "images.kabum.com.br" },
-      { hostname: "dcdn.mitiendanube.com" },
-      { hostname: "store.storeimages.cdn-apple.com" },
-      { hostname: "loja.kyojin.com.br" },
-      { hostname: "i.zst.com.br" },
-      { hostname: "d3ddx6b2p2pevg.cloudfront.net" },
-    ],
+    remotePatterns: [{ hostname: "imgur.com" }],
   },
 };
 
