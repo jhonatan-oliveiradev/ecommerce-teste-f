c128fca9f4e3b66a0b49059b4c44128860e563fc
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-06-06 18:49:15 -0300

    chore: added a hostname to image domains

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    remotePatterns: [{ hostname: "imgur.com" }],
+    remotePatterns: [{ hostname: "imgur.com" }, { hostname: "i.imgur.com" }],
   },
 };
 
