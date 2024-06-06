51a987292bc4228f5c632484e3944f716b84b3cb
jhonatan-oliveiradev <jhonatan.oliveira931002@gmail.com>
2024-06-06 18:01:05 -0300

    feat: side menu adjusts

--- src/components/side-menu.tsx
+++ src/components/side-menu.tsx
@@ -1,16 +1,15 @@
-import { MenuIcon } from "lucide-react";
-import { Button } from "./ui/button";
+import { ArrowLeftFromLineIcon, ShoppingCartIcon } from "lucide-react";
 import {
   Sheet,
   SheetContent,
   SheetDescription,
-  SheetFooter,
   SheetHeader,
   SheetTitle,
   SheetTrigger,
 } from "@/components/ui/sheet";
-import ProductResume from "@/app/cart/components/product-resume";
 import CategoryItem from "./category-item";
+import { Button } from "./ui/button";
+import Link from "next/link";
 
 interface SideMenuProps {
   categories: {
@@ -22,26 +21,21 @@ interface SideMenuProps {
 const SideMenu = ({ categories }: SideMenuProps) => {
   return (
     <Sheet>
-      <Button size="icon">
-        <SheetTrigger>
-          <MenuIcon size={16} />
-        </SheetTrigger>
-      </Button>
+      <SheetTrigger className="rounded-lg bg-primary p-2 text-white">
+        <ArrowLeftFromLineIcon size={16} />
+      </SheetTrigger>
       <SheetContent>
         <SheetHeader>
           <SheetTitle className="uppercase">Categorias</SheetTitle>
-          <SheetDescription className="pb-2 text-xs text-muted-foreground">
+          <SheetDescription className="pb-4 text-xs text-muted-foreground">
             Navegue através das categorias para encontrar o produto que procura.
           </SheetDescription>
         </SheetHeader>
-        <div className="max-h-[calc(100vh - 550px)] flex flex-col gap-1 overflow-y-scroll">
+        <div className="max-h-[calc(100vh - 550px)] flex flex-col gap-4 overflow-y-scroll">
           {categories.map((category) => (
             <CategoryItem key={category.slug} category={category} />
           ))}
         </div>
-        {/* <SheetFooter className="fixed bottom-0 right-1 flex items-center">
-          <ProductResume />
-        </SheetFooter> */}
       </SheetContent>
     </Sheet>
   );
